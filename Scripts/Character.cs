7546616baa59b7604c0b67324664ce7195fe7c5a
Harrison <harrisonhough4@gmail.com>
2022-10-03 10:58:34 +0300

    added script

---
+++ Scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PluginB
+{
+    public class Character : MonoBehaviour
+    {
+        [SerializeField]private string name = "Kevin";
+        [SerializeField] private int maxHealth = 100;
+
+        private void Start()
+        {
+            Debug.Log($"Game starts for {name}. Healh = {maxHealth}");
+        }
+    }
+}
